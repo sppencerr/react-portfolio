8eb9ee1878f6158fe5954e1a7cf5bbba843d103b
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-02 00:59:45 -0700

    navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,1 +1,36 @@
 import React from 'react';
+
+const NavBar = () => {
+  return (
+    <body background="./assets/Images/aquablueBackground.jpeg">
+      <section id="personal-info">
+        <nav>
+          <div className="website__creator">Spencer Gerritsen</div>
+          <ul className="navbar__link--list">
+            <li className="navbar__link">
+              <a href="#languages" className="
+                navbar__link--anch
+                link__hover-effect
+                link__hover-effect--black
+              ">Languages</a>
+            </li>
+            <li className="navbar__link">
+              <a href="#projects" className="
+                navbar__link--anch
+                link__hover-effect
+                link__hover-effect--black
+              ">Projects</a>
+            </li>
+            <li className="navbar__link">
+              <a href="mailto:spencergerritsen@gmail.com" className="
+                navbar__link--anch
+                navbar__link--anch-main">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </body>
+  );
+}
+
+export default NavBar;
